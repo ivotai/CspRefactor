af6be5bbfa55bdf896dc44bc3febf55bf19856a5
ivotai <ivotai@163.com>
2021-08-21 07:24:58 +0800

    登录布局完成。

--- app/src/main/java/com/unircorn/csp/ui/fra/LoginFra.kt
+++ app/src/main/java/com/unircorn/csp/ui/fra/LoginFra.kt
@@ -1,14 +1,11 @@
 package com.unircorn.csp.ui.fra
 
-import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.mikepenz.iconics.IconicsDrawable
 import com.mikepenz.iconics.typeface.library.fontawesome.FontAwesome
-import com.mikepenz.iconics.utils.colorInt
 import com.mikepenz.iconics.utils.sizeDp
 import com.unircorn.csp.R
 import com.unircorn.csp.databinding.FraLoginBinding
@@ -17,19 +14,23 @@ import com.unircorn.csp.ui.base.BaseFra
 class LoginFra : BaseFra(R.layout.fra_login) {
 
     override fun initViews() {
-        val i =IconicsDrawable(requireContext(), FontAwesome.Icon.faw_android).apply {
-            colorInt = Color.RED
-            sizeDp = 24
+        with(binding) {
+            tilUsername.startIconDrawable =
+                IconicsDrawable(requireContext(), FontAwesome.Icon.faw_user1).apply {
+                    sizeDp = 24
+                }
+            tilPassword.startIconDrawable =
+                IconicsDrawable(requireContext(), FontAwesome.Icon.faw_lock).apply {
+                    sizeDp = 24
+                }
         }
-        binding.textField.startIconDrawable = i
-
     }
-
 
 
     // ----
 
     private var _binding: FraLoginBinding? = null
+
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
