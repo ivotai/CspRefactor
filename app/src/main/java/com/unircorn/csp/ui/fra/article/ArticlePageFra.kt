20c62348407bd2bb2fc25238577145137f07f9d5
ivotai <ivotai@163.com>
2021-09-05 10:09:46 +0800

    my and search

--- app/src/main/java/com/unircorn/csp/ui/fra/article/ArticlePageFra.kt
+++ app/src/main/java/com/unircorn/csp/ui/fra/article/ArticlePageFra.kt
@@ -1,33 +1,35 @@
 package com.unircorn.csp.ui.fra.article
 
+import android.content.Intent
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.chad.library.adapter.base.entity.MultiItemEntity
-import com.unircorn.csp.app.Category
-import com.unircorn.csp.app.Title
+import com.unircorn.csp.app.*
 import com.unircorn.csp.data.model.base.Page
 import com.unircorn.csp.data.model.base.Response
-import com.unircorn.csp.databinding.UiPageBinding
+import com.unircorn.csp.databinding.FraArticlePageBinding
+import com.unircorn.csp.ui.act.article.ArticleSearchAct
+import com.unircorn.csp.ui.act.my.MyAct
 import com.unircorn.csp.ui.adapter.ArticleAdapter
 import com.unircorn.csp.ui.base.PageFra2
 import io.reactivex.rxjava3.core.Single
 
-open class ArticlePageFra : PageFra2<UiPageBinding, MultiItemEntity>() {
+open class ArticlePageFra : PageFra2<FraArticlePageBinding, MultiItemEntity>() {
 
     override fun initViews() = with(binding) {
         super.initViews()
         tvTitle.text = title
     }
 
-    override fun initBindings() {
+    override fun initBindings(): Unit = with(binding) {
         super.initBindings()
-        // todo search and my
-//        Intent(requireContext(), ArticleSearchAct::class.java)
-//            .apply { putExtra(Param, category) }
-//            .let { requireContext().startActivity(it) }
-
-//        startAct(MyAct::class.java)
+        ivSearch.safeClicks().subscribe {
+            Intent(requireContext(), ArticleSearchAct::class.java)
+                .apply { putExtra(Param, category) }
+                .let { requireContext().startActivity(it) }
+        }
+        ivMy.safeClicks().subscribe { startAct(MyAct::class.java) }
     }
 
     override fun initPageAdapter() {
