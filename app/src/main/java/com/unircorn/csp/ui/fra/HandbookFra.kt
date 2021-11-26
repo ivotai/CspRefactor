2f6d838e55b1471692acba26df551872d0692fa4
ivotai <ivotai@163.com>
2021-11-26 23:19:04 +0800

    待明天调试布局。

--- app/src/main/java/com/unircorn/csp/ui/fra/HandbookFra.kt
+++ app/src/main/java/com/unircorn/csp/ui/fra/HandbookFra.kt
@@ -1,25 +1,45 @@
 package com.unircorn.csp.ui.fra
 
-import com.google.android.material.tabs.TabLayoutMediator
+import android.content.Intent
+import com.ruffian.library.widget.RTextView
+import com.unircorn.csp.app.Category
+import com.unircorn.csp.app.Title
+import com.unircorn.csp.app.safeClicks
 import com.unircorn.csp.databinding.FraHandbookBinding
-import com.unircorn.csp.databinding.FraStudyBinding
+import com.unircorn.csp.ui.act.article.HandbookArticleAct
 import com.unircorn.csp.ui.base.BaseFra2
-import com.unircorn.csp.ui.pagerAdapter.HandbookPagerAdapter
-import com.unircorn.csp.ui.pagerAdapter.StudyPagerAdapter
 
 class HandbookFra : BaseFra2<FraHandbookBinding>() {
 
+    private val titles = listOf(
+        "机关作风", "三个规定", "\"两违\"\"两官\"", "立案信访", "审限管理",
+        "减刑假释", "暂予监外执行", "民事审判", "破产管理", "行政审判",
+        "执行管理"
+    )
+    private val mCategories = listOf(
+        "sbs_jgzf", "sbs_sggd", "sbs_lwlg", "sbs_laxf", "sbs_sxgl",
+        "sbs_jxjs", "sbs_zyjwzx", "sbs_mssp", "sbs_pcgl", "sbs_xzsp",
+        "sbs_zxgl"
+    )
+
+    private lateinit var tvLabels: List<RTextView>
+
     override fun initViews() = with(binding) {
-        tvTitle.text = "手编书"
-        initTabLayoutMediator()
+        tvLabels = listOf(tvLabel1, tvLabel2, tvLabel3, tvLabel4)
+        tvLabels.forEachIndexed { index, rTextView ->
+            rTextView.text = titles[index]
+        }
     }
 
-    private fun initTabLayoutMediator() = with(binding) {
-        viewPager2.adapter = HandbookPagerAdapter(this@HandbookFra)
-        viewPager2.offscreenPageLimit = HandbookPagerAdapter.titles.size - 1
-        TabLayoutMediator(tabLayout, viewPager2) { tab, position ->
-            tab.text = HandbookPagerAdapter.titles[position]
-        }.attach()
+    override fun initBindings() {
+        tvLabels.forEachIndexed { index, rTextView ->
+            rTextView.safeClicks().subscribe {
+                startActivity(Intent(requireContext(), HandbookArticleAct::class.java).apply {
+                    putExtra(Title, titles[index])
+                    putExtra(Category, mCategories[index])
+                })
+            }
+        }
     }
 
 }
