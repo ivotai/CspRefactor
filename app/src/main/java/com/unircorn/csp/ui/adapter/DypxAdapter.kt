2f7f436583caa9c52ff818e87335ec49308514ae
ivotai <ivotai@163.com>
2022-04-27 23:13:53 +0800

    专题点击跳转。

--- app/src/main/java/com/unircorn/csp/ui/adapter/DypxAdapter.kt
+++ app/src/main/java/com/unircorn/csp/ui/adapter/DypxAdapter.kt
@@ -1,18 +1,28 @@
 package com.unircorn.csp.ui.adapter
 
+import android.content.Intent
+import android.view.View
 import com.chad.library.adapter.base.BaseQuickAdapter
-import com.chad.library.adapter.base.module.LoadMoreModule
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
 import com.unircorn.csp.R
+import com.unircorn.csp.app.Category
+import com.unircorn.csp.app.Title
+import com.unircorn.csp.app.safeClicks
 import com.unircorn.csp.data.model.Code
-import org.ocpsoft.prettytime.PrettyTime
+import com.unircorn.csp.ui.act.article.ArticlePageAct
 
-class DypxAdapter : BaseQuickAdapter<Code, BaseViewHolder>(R.layout.item_dypx),
-    LoadMoreModule {
+class DypxAdapter : BaseQuickAdapter<Code, BaseViewHolder>(R.layout.item_dypx) {
 
     override fun convert(holder: BaseViewHolder, item: Code) {
         holder.apply {
             setText(R.id.tvName, item.name)
+
+            getView<View>(R.id.root).safeClicks().subscribe {
+                context.startActivity(Intent(context, ArticlePageAct::class.java).apply {
+                    putExtra(Title, item.name)
+                    putExtra(Category, item.objectId)
+                })
+            }
         }
     }
 
