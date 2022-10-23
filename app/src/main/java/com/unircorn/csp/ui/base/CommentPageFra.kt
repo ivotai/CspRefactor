157817fc5c422c54432be313c0bdc837b310d62c
ivotai <ivotai@163.com>
2022-10-23 11:32:05 +0800

    添加点赞按钮和事件。

--- app/src/main/java/com/unircorn/csp/ui/base/CommentPageFra.kt
+++ app/src/main/java/com/unircorn/csp/ui/base/CommentPageFra.kt
@@ -2,18 +2,27 @@ package com.unircorn.csp.ui.base
 
 import com.blankj.utilcode.util.ToastUtils
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.textfield.TextInputEditText
+import com.mikepenz.iconics.IconicsDrawable
+import com.mikepenz.iconics.utils.sizeDp
+import com.unicorn.sanre.icon.Fas
 import com.unircorn.csp.app.isEmpty
 import com.unircorn.csp.app.safeClicks
 import com.unircorn.csp.data.model.Comment
 import com.unircorn.csp.ui.adapter.CommentAdapter
-import com.unircorn.csp.ui.base.PageFra
 
 abstract class CommentPageFra : PageFra<Comment>() {
+
+    override fun initViews() {
+        super.initViews()
+        initFloatingActionButton()
+    }
 
     override fun initBindings() {
         super.initBindings()
         btnCreateComment.safeClicks().subscribe { createCommentX() }
+        floatingActionButton.safeClicks().subscribe { like() }
     }
 
     override fun initPageAdapter() {
@@ -33,10 +42,24 @@ abstract class CommentPageFra : PageFra<Comment>() {
         loadStartPage()
     }
 
+    open fun like(){
+
+    }
+
+    private fun initFloatingActionButton() {
+        floatingActionButton.setImageDrawable(
+            IconicsDrawable(requireContext(), Fas.Icon.fas_thumbs_up).apply {
+                sizeDp = 24
+            }
+        )
+    }
+
     abstract fun createComment()
 
     abstract val etContent: TextInputEditText
 
     abstract val btnCreateComment: MaterialButton
+
+    abstract val floatingActionButton: FloatingActionButton
 
 }
